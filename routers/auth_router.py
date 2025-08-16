4195bc3a052fed78a8d4a53ea7bf2a0b5c7d98ce
Carlos <cvdejesuss.bit@gmail.com>
2025-08-15 22:09:34 -0300

    implement JWT authentication and route protection

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -3,16 +3,21 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models.user import User
 from pydantic import BaseModel
+from auth.jwt_handler import create_access_token
 
-router = APIRouter()
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
 
 class LoginRequest(BaseModel):
     email: str
     password: str
+
 
 @router.post("/auth/login")
 def login(data: LoginRequest, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == data.email).first()
     if not user or user.password != data.password:
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    return {"message": "Login successful", "user_id": user.id}
+
+    token = create_access_token({"sub": str(user.id)})
+    return {"access_token": token, "token_type": "bearer"}
--- routers/record_router.py
+++ routers/record_router.py
@@ -5,6 +5,7 @@ from models.record import MedicalRecord
 from models.patient import Patient
 from schemas import RecordCreate, RecordOut
 from datetime import datetime, timezone
+from auth.jwt_bearer import get_current_user
 
 router = APIRouter(
     prefix="/patients",
@@ -15,7 +16,8 @@ router = APIRouter(
 def create_record(
     patient_id: int,
     record: RecordCreate,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    user_data: dict = Depends(get_current_user)  # 👈 Aqui protegemos a rota
 ):
     patient = db.query(Patient).filter(Patient.id == patient_id).first()
     if not patient:
@@ -32,6 +34,10 @@ def create_record(
     return db_record
 
 @router.get("/{patient_id}/records", response_model=list[RecordOut])
-def get_records(patient_id: int, db: Session = Depends(get_db)):
+def get_records(
+    patient_id: int,
+    db: Session = Depends(get_db),
+    user_data: dict = Depends(get_current_user)
+):
     records = db.query(MedicalRecord).filter(MedicalRecord.patient_id == patient_id).all()
     return records
